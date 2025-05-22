96510bd3acb3080df2a680f71fd40b0932ce4973
linrongbin16 <linrongbin16@outlook.com>
2025-05-22 13:46:40 +0800

    chore

--- tests/unicode/gen_ascii_control_chars.py
+++ tests/unicode/gen_ascii_control_chars.py
@@ -5,5 +5,5 @@
 
 control_chars = [i for i in range(32)]
 
-with open("ascii-control-chars.txt", mode="wb") as f:
+with open("ascii_control_chars.txt", mode="wb") as f:
     f.write(bytes(control_chars))
